01a560ada04b39909aba54d93de845b6f2f4a21b
chuasonglin1995 <chuasonglin1995@gmail.com>
2024-10-05 19:03:38 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,19 @@ use std::env;
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
-    let first_char = encoded_value.chars().next().expect("Empty string received");
 
-    match first_char {
+    match encoded_value.chars().next() {
         // decode integers eg. i52e
-        'i' => {
+        Some('i') => {
             if let Some((n, rest)) = encoded_value.strip_prefix('i').unwrap().split_once('e') {
                  if let Ok(n) = n.parse::<i64>() {
                     return (n.into(), rest)
-                 } else {
-                    panic!("integer cannot be decoded")
                  }
-            } else {
-                panic!("integer cannot be decoded")
-            }
+            } 
         }
 
-        // decode lists eg. l5:helloe = ["hello"], li25el3:fooi-43e5:helloe = [25,[ "foo", -43], "hello"]
-        'l' => {
+        // decode lists eg. l5:helloe = ["hello"], li25el3:fooi-43ee5:helloe = [25,[ "foo", -43], "hello"]
+        Some('l') => {
             let mut values = Vec::new();
             let mut rest = encoded_value.split_at(1).1;
             while !rest.is_empty() && rest.chars().next().unwrap() != 'e' {
@@ -34,26 +29,41 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
                 rest = remainder;
             }
             return (values.into(), &rest[1..])
+        }
 
+        // decode dictionaries eg. d3:foo3:bar5:helloi52ee = {"foo": "bar", "hello": 52}
+        Some('d') => {
+            let mut dict = serde_json::Map::new();
+            let mut rest = encoded_value.split_at(1).1;
+            while !rest.is_empty() && rest.chars().next().unwrap() != 'e' {
+                let (k, remainder) = decode_bencoded_value(rest);
+                let k = match k {
+                    serde_json::Value::String(k) => k,
+                    k => {
+                        panic!("dict keys must be strings, not {k:?}")
+                    }
+                };
+                let (v, remainder) = decode_bencoded_value(remainder);
+                eprint!("k: {k}, v {v}\n");
+                dict.insert(k, v);
+                rest = remainder;
+            }
+            return (dict.into(), &rest[1..])
         }
 
         // decode strings eg. 5:hello
-        '0'..='9' => {
+        Some('0'..='9') => {
             if let Some((len, str)) = encoded_value.split_once(':') {
                 if let Ok(len) = len.parse::<usize>() {
-                    return (str[..len].into(), &str[len..]);
-                } else {
-                    panic!("string length cannot be parsed")
+                    return (str[..len].to_string().into(), &str[len..]);
                 }
-            } else {
-                panic!("string cannot be decoded")
             }
         }
 
-        _ => {
-            panic!("Unhandled encoded value: {}", encoded_value);
-        }
+        _ => {}
+        
     }
+    panic!("Unhandled encoded value: {}", encoded_value);
 
 }
 
